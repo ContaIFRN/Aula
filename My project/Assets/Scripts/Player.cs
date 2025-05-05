5396a9795c67ab2d1e341b651577e132425906d8
ContaIFRN <rodrigues.morais@escolar.ifrn.edu.br>
2025-05-05 12:08:00 -0300

    coisa

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int numero = 0;
+    public float velocidade;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        numero = 0;
+    }
+
+    {
+        void Update()
+        //Debug.Log(numero);
+        //Debug = numero +1;
+    
+    
+    
