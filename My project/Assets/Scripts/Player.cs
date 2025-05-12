58385c2d925bc8ded57d2b1c75c68310b122ffa5
ContaIFRN <rodrigues.morais@escolar.ifrn.edu.br>
2025-05-12 11:55:12 -0300

    CONSEGUI FAZER MOVIMENTO
    sou foda

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -3,18 +3,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private int numero = 0;
-    public float velocidade;
+    private int numero;
+
+    public float velocidade = 30;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         numero = 0;
     }
 
+// update is called once per frame
+    void Update()
     {
-        void Update()
-        //Debug.Log(numero);
-        //Debug = numero +1;
-    
-    
-    
+        // Debug.Log(numero);
+        // numero = numero +1
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
+        }
+    }
+}
